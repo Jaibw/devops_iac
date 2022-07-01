f04d63006a928935c97178cc27ef2067e2256a25
Jaibw <jai.unix@gmail.com>
2022-07-02 03:13:00 +0530

    Update create_instance.py

--- python_code/create_instance.py
+++ python_code/create_instance.py
@@ -6,7 +6,7 @@ num_instances = 1
 keyName = "user00"
 imageid = 'ami-01154c8b2e9a14885'
 instancetype = 't2.micro'
-
+print("Creating Instance: ", machine_tag)
 ec2 = boto3.resource('ec2')
 instance = ec2.create_instances(
                 ImageId=imageid,
@@ -14,9 +14,11 @@ instance = ec2.create_instances(
                 MaxCount=num_instances,
                 InstanceType=instancetype,
                 KeyName=keyName )
+for x1 in instance: 
+  x1.create_tags(Tags=[{"Key": "Name", "Value": machine_tag}])
 
-instance.create_tags(Tags=[{"Key": "Name", "Value": machine_tag}])
-
-instance.wait_until_running()
-print(instance.state)
-print(instance.instance_id, instance.public_ip_address)
+for x1 in instance:
+  print("Waiting for running state")
+  x1.wait_until_running()
+  print("State: ",x1.state)
+  print("Details: ",x1.instance_id, x1.public_ip_address)
