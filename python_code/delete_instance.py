6ec2d32738d00d5b0e0045b9acc89bbb132dd147
Jaibw <jai.unix@gmail.com>
2022-07-02 03:30:28 +0530

    Update delete_instance.py

--- python_code/delete_instance.py
+++ python_code/delete_instance.py
@@ -1,6 +1,7 @@
 import boto3
 newlist=[]
 ec2= boto3.resource('ec2')
+ec2_client = boto3.client('ec2')
 instances= ec2.meta.client.describe_instances()
 for x1 in instances['Reservations']: newlist.append(x1['Instances'][0]['InstanceId'])
-ec2.terminate_instances(InstanceIds=(newlist))
+ec2_client.terminate_instances(InstanceIds=(newlist))
