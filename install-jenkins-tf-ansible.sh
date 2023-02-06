b8abddc3e717000104a9d4537b97849a4db54155
Jaibw <jai.unix@gmail.com>
2023-02-06 12:41:14 +0530

    Update install-jenkins-tf-ansible.sh

--- install-jenkins-tf-ansible.sh
+++ install-jenkins-tf-ansible.sh
@@ -13,4 +13,4 @@ wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform -y 
 
-sudo apt install ansible -y
+sudo apt install ansible awscli -y
