c190490f07332926ad382ab465f1514243e655ce
Jaibw <jai.unix@gmail.com>
2023-02-06 12:37:55 +0530

    Update install-jenkins-tf-ansible.sh

--- install-jenkins-tf-ansible.sh
+++ install-jenkins-tf-ansible.sh
@@ -1,11 +1,12 @@
-apt update 
-sudo apt install openjdk-11-jre -y
+sudo apt update -y
+
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
+sudo apt-get update 
+sudo apt-get install openjdk-11-jre -y
 sudo apt-get install jenkins -y 
 
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
